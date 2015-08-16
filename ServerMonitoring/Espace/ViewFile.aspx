2ca75f2374371e88d485244600f8fe2606a77804
Sylvain Lemoine <s.lemoine@izysolutions.com>
2015-08-16 18:11:21 +0200

    view file

--- ServerMonitoring/Espace/ViewFile.aspx
+++ ServerMonitoring/Espace/ViewFile.aspx
@@ -1,107 +1,103 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ViewFile.aspx.cs" Inherits="Espace_ViewFile" MasterPageFile="~/MP/MP.master" %>
 
 <asp:Content runat="server" ContentPlaceHolderID="head">
-	<link rel="stylesheet" type="text/css" href="../CSS/viewfile.css" />
+    <link rel="stylesheet" type="text/css" href="../CSS/viewfile.css" />
 </asp:Content>
 
 <asp:Content runat="server" ContentPlaceHolderID="Content">
-	<script>
-		function ShowFileDelay() {
-			setTimeout(ShowFile, 50);
-		}
+    <script>
+        function ShowFileDelay() {
+            setTimeout(ShowFile, 50);
+        }
 
-		function ShowFile() {
-			$(".liste-fichier-contenu").addClass("cache");
-			$(".fichier-contenu").addClass("visible");
-			$(".btn-back").addClass("visible");
-			console.log("showfile");
-		}
+        function ShowFile() {
+            $(".liste-fichier-contenu").addClass("cache");
+            $(".fichier-contenu").addClass("visible");
+            $(".btn-back").addClass("visible");
+            console.log("showfile");
+        }
 
 
-		function HideFileDelay() {
-			console.log("hide file delay");
-			setTimeout(HideFile, 1000);
-		}
+        function HideFileDelay() {
+            console.log("hide file delay");
+            setTimeout(HideFile, 1000);
+        }
 
-		function HideFile() {
-			console.log("hide file ");
-			$(".liste-fichier-contenu").removeClass("cache");
-			$(".fichier-contenu").removeClass("visible");
-			$(".btn-back").removeClass("visible");
-			console.log("hidefile");
-		}
+        function HideFile() {
+            console.log("hide file ");
+            $(".liste-fichier-contenu").removeClass("cache");
+            $(".fichier-contenu").removeClass("visible");
+            $(".btn-back").removeClass("visible");
+            console.log("hidefile");
+        }
 
-		function OpenCloseHeader() {
-			$(".view-file-header").toggleClass("open");
-		}
-	</script>
+        function OpenCloseHeader() {
+            $(".view-file-header").toggleClass("open");
+        }
+    </script>
 
-	<asp:Panel runat="server" ID="pnlHeader" CssClass="view-file-header">
-		<asp:LinkButton runat="server" CssClass="btn-back" OnClick="btnGoBackListe_Click">
+    <asp:Panel runat="server" ID="pnlHeader" CssClass="view-file-header">
+        <asp:LinkButton runat="server" CssClass="btn-back" OnClick="btnGoBackListe_Click">
 			<i class="fa fa-arrow-circle-left fa-2x"></i>
-		</asp:LinkButton>
+        </asp:LinkButton>
 
-		<asp:Label runat="server" ID="lblNomFichier" CssClass="view-file-header-libelle"></asp:Label>
-		&nbsp;
+        <asp:Label runat="server" ID="lblNomFichier" CssClass="view-file-header-libelle"></asp:Label>
+        &nbsp;
 		<i runat="server" id="icon_folder" visible="false" class="fa fa-folder-o fa-lg" title="Répertoire"></i>
-		<i runat="server" id="icon_file" visible="false" class="fa fa-file-text-o fa-lg" title="Fichier"></i>
-		<asp:Image runat="server" ImageUrl="~/img/arrow-file.png" CssClass="header-arrow" onclick="OpenCloseHeader()" />
-		<br />
-		<asp:Label runat="server" ID="lblCheminFichier" CssClass="view-file-header-chemin"></asp:Label>
-		<br />
-		<asp:Label runat="server" ID="lblCommentaire" CssClass="view-file-header-chemin"></asp:Label>
+        <i runat="server" id="icon_file" visible="false" class="fa fa-file-text-o fa-lg" title="Fichier"></i>
+        <asp:Image runat="server" ImageUrl="~/img/arrow-file.png" CssClass="header-arrow" onclick="OpenCloseHeader()" />
+        <br />
+        <asp:Label runat="server" ID="lblCheminFichier" CssClass="view-file-header-chemin"></asp:Label>
+        <br />
+        <asp:Label runat="server" ID="lblCommentaire" CssClass="view-file-header-chemin"></asp:Label>
 
 
-	</asp:Panel>
-	<div class="header-space"></div>
+    </asp:Panel>
+    <div class="header-space"></div>
 
-	<asp:UpdatePanel runat="server" ID="upGeneral" UpdateMode="Conditional">
-		<ContentTemplate>
+    <asp:UpdatePanel runat="server" ID="upGeneral" UpdateMode="Conditional">
+        <ContentTemplate>
 
-			<asp:UpdatePanel runat="server" ID="upListes" UpdateMode="Conditional">
-				<ContentTemplate>
-					<asp:Panel runat="server" ID="pnlListeFichier" CssClass="liste-fichier-contenu">
+            <asp:UpdatePanel runat="server" ID="upListes" UpdateMode="Conditional">
+                <ContentTemplate>
+                    <asp:Panel runat="server" ID="pnlListeFichier" CssClass="liste-fichier-contenu">
 
-						<asp:Repeater runat="server" ID="rptListeFolder">
-							<ItemTemplate>
-								<asp:LinkButton runat="server" ID="btnGoFolder" CssClass="fichier" CommandArgument='<%# Eval("chemin") %>' OnClick="btnGoFolder_Click">
-							<i class="fa fa-folder-o fa-lg" title="Répertoire"></i>
-							<asp:Label runat="server" Text='<%# Eval("nom") %>'></asp:Label>
-							<%--<div class="arrow-container">
-								<i class="fa fa-arrow-right"></i>
-							</div>--%>
-								</asp:LinkButton>
-								<br />
-							</ItemTemplate>
-						</asp:Repeater>
+                        <asp:Repeater runat="server" ID="rptListeFolder">
+                            <ItemTemplate>
+                                <asp:LinkButton runat="server" ID="btnGoFolder" CssClass="fichier" CommandArgument='<%# Eval("chemin") %>' OnClick="btnGoFolder_Click">
+							        <i class="fa fa-folder-o fa-lg" title="Répertoire"></i>
+							        <asp:Label runat="server" Text='<%# Eval("nom") %>'></asp:Label>
+                                </asp:LinkButton>
+                                <br />
+                            </ItemTemplate>
+                        </asp:Repeater>
 
-						<asp:Repeater runat="server" ID="rptListeFichiers">
-							<ItemTemplate>
-								<asp:LinkButton runat="server" ID="btnSeeFichier" CssClass="fichier" CommandArgument='<%# Eval("chemin") %>' OnClick="btnSeeFichier_Click">
-							<i class="fa fa-file-text-o fa-lg" title="Fichier"></i>
-							<asp:Label runat="server" Text='<%# Eval("nom") %>'></asp:Label>
-							<%--<div class="arrow-container">
-								<i class="fa fa-arrow-right"></i>
-							</div>--%>
-								</asp:LinkButton>
-								<br />
-							</ItemTemplate>
-						</asp:Repeater>
+                        <asp:Repeater runat="server" ID="rptListeFichiers">
+                            <ItemTemplate>
+                                <asp:LinkButton runat="server" ID="btnSeeFichier" CssClass="fichier" CommandArgument='<%# Eval("chemin") %>' OnClick="btnSeeFichier_Click">
+                                    <i class="fa fa-file-text-o fa-lg" title="Fichier"></i>
+                                    <asp:Label runat="server" Text='<%# Eval("nom") %>'></asp:Label>&nbsp;
+							        <asp:Label runat="server" CssClass="fichier-size" Text='<%# Eval("taille") %>'></asp:Label>
 
-					</asp:Panel>
-				</ContentTemplate>
-			</asp:UpdatePanel>
+                                </asp:LinkButton>
+                                <br />
+                            </ItemTemplate>
+                        </asp:Repeater>
 
-			<asp:UpdatePanel runat="server" ID="upFile" UpdateMode="Conditional">
-				<ContentTemplate>
-					<asp:Panel runat="server" ID="pnlContenuFichier" CssClass="fichier-contenu">
-						<pre>
+                    </asp:Panel>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+
+            <asp:UpdatePanel runat="server" ID="upFile" UpdateMode="Conditional">
+                <ContentTemplate>
+                    <asp:Panel runat="server" ID="pnlContenuFichier" CssClass="fichier-contenu">
+                        <pre>
 <asp:Literal runat="server" ID="litFileContent"></asp:Literal>
 	</pre>
-					</asp:Panel>
-				</ContentTemplate>
-			</asp:UpdatePanel>
+                    </asp:Panel>
+                </ContentTemplate>
+            </asp:UpdatePanel>
 
-		</ContentTemplate>
-	</asp:UpdatePanel>
+        </ContentTemplate>
+    </asp:UpdatePanel>
 </asp:Content>
--- ServerMonitoring/Espace/ViewFile.aspx.cs
+++ ServerMonitoring/Espace/ViewFile.aspx.cs
@@ -11,127 +11,176 @@ using iZyTools.Convertion;
 
 public partial class Espace_ViewFile : BasePage
 {
-	protected void Page_Load(object sender, EventArgs e)
-	{
-		Utilisateur myUser = GetUtilisateur();
-		if (myUser != null)
-		{
-			if (!IsPostBack)
-			{
-				int idLog = iZyInt.ConvertStringToInt(MySession.GetParam("idLog"));
-				Log myLog = LogManager.Load(idLog);
-				if (myLog != null)
-				{
-					lblNomFichier.Text = myLog.myProjet.libelle + " - " + myLog.libelle;
-					lblCheminFichier.Text = myLog.cheminFichier;
-					lblCommentaire.Text = myLog.commentaire;
-					pnlHeader.CssClass += " " + myLog.myProjet.myTheme.cssClass;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Utilisateur myUser = GetUtilisateur();
+        if (myUser != null)
+        {
+            if (!IsPostBack)
+            {
+                int idLog = iZyInt.ConvertStringToInt(MySession.GetParam("idLog"));
+                Log myLog = LogManager.Load(idLog);
+                if (myLog != null)
+                {
+                    lblNomFichier.Text = myLog.myProjet.libelle + " - " + myLog.libelle;
+                    lblCheminFichier.Text = myLog.cheminFichier;
+                    lblCommentaire.Text = myLog.commentaire;
+                    pnlHeader.CssClass += " " + myLog.myProjet.myTheme.cssClass;
 
-					bool? isDirectory = IsDirectory(myLog.cheminFichier);
-					if (isDirectory.HasValue)
-					{
-						if (isDirectory.Value) // c'est un répertoire
-						{
-							icon_folder.Visible = true;
-							pnlListeFichier.Visible = true;
+                    bool? isDirectory = IsDirectory(myLog.cheminFichier);
+                    if (isDirectory.HasValue)
+                    {
+                        if (isDirectory.Value) // c'est un répertoire
+                        {
+                            icon_folder.Visible = true;
+                            pnlListeFichier.Visible = true;
 
-							pnlListeFichier.CssClass += " " + myLog.myProjet.myTheme.cssClass;
-							DisplayListeFichiers(myLog.cheminFichier);
-						}
-						else // c'est un fichier
-						{
-							pnlListeFichier.Visible = false;
-							pnlContenuFichier.CssClass += " visible";
+                            pnlListeFichier.CssClass += " " + myLog.myProjet.myTheme.cssClass;
+                            DisplayListeFichiers(myLog.cheminFichier);
+                        }
+                        else // c'est un fichier
+                        {
+                            pnlListeFichier.Visible = false;
+                            pnlContenuFichier.CssClass += " visible";
 
-							icon_file.Visible = true;
-							DisplayFichierContenu(myLog.cheminFichier);
-						}
-					}
-					else
-					{
-						litFileContent.Text = "Le fichier n'existe pas ou le chemin est inaccessible";
-					}
-				}
-			}
-		}
-		else
-			Response.Redirect("~/Default.aspx");
-	}
+                            icon_file.Visible = true;
+                            DisplayFichierContenu(myLog.cheminFichier);
+                        }
+                    }
+                    else
+                    {
+                        litFileContent.Text = "Le fichier n'existe pas ou le chemin est inaccessible";
+                    }
+                }
+            }
+        }
+        else
+            Response.Redirect("~/Default.aspx");
+    }
 
-	public bool? IsDirectory(string path)
-	{
-		if (Directory.Exists(path))
-			return true; // is a directory 
-		else if (File.Exists(path))
-			return false; // is a file 
-		else
-			return null; // is a nothing 
-	}
+    public bool? IsDirectory(string path)
+    {
+        if (Directory.Exists(path))
+            return true; // is a directory 
+        else if (File.Exists(path))
+            return false; // is a file 
+        else
+            return null; // is a nothing 
+    }
 
-	public void DisplayFichierContenu(string cheminFichier)
-	{
-		if (File.Exists(cheminFichier))
-			litFileContent.Text = File.ReadAllText(cheminFichier);
-		else
-			litFileContent.Text = "Le fichier n'existe pas ou le chemin est inaccessible";
-	}
-
-
-
-	public class Fichier
-	{
-		public string chemin { get; set; }
-		public string nom { get; set; }
-	}
-
-	public class Repertoire
-	{
-		public string chemin { get; set; }
-		public string nom { get; set; }
-	}
-
-	protected void DisplayListeFichiers(string cheminRepertoire)
-	{
-		if (Directory.Exists(cheminRepertoire))
-		{
-			string[] arrayFichiers = Directory.GetFiles(cheminRepertoire);
-
-			List<Fichier> listeFichiers = arrayFichiers.Select(f => new Fichier() { chemin = f, nom = Path.GetFileName(f) }).ToList();
-			rptListeFichiers.DataSource = listeFichiers.OrderBy(f => f.nom);
-			rptListeFichiers.DataBind();
+    public void DisplayFichierContenu(string cheminFichier)
+    {
+        if (File.Exists(cheminFichier))
+            litFileContent.Text = File.ReadAllText(cheminFichier);
+        else
+            litFileContent.Text = "Le fichier n'existe pas ou le chemin est inaccessible";
+    }
 
 
-			string[] listeFolder = Directory.GetDirectories(cheminRepertoire);
-			List<Repertoire> listeRepertoire = listeFolder.Select(f => new Repertoire() { chemin = f, nom = Path.GetFileName(f) }).ToList();
-			rptListeFolder.DataSource = listeRepertoire.OrderBy(f => f.nom);
-			rptListeFolder.DataBind();
-		}
-	}
 
-	protected void btnSeeFichier_Click(object sender, EventArgs e)
-	{
-		LinkButton btn = (LinkButton)sender;
-		if (btn != null && !string.IsNullOrEmpty(btn.CommandArgument))
-		{
-			DisplayFichierContenu(btn.CommandArgument);
+    public class Fichier
+    {
+        public string chemin { get; set; }
+        public string nom { get; set; }
+        public string taille { get; set; }
+    }
 
-			ScriptManager.RegisterStartupScript(this, this.GetType(), "ShowFileDelay", "ShowFileDelay();", true);
-			upFile.Update();
-		}
-	}
-	protected void btnGoFolder_Click(object sender, EventArgs e)
-	{
+    public class Repertoire
+    {
+        public string chemin { get; set; }
+        public string nom { get; set; } 
+    }
 
-	}
-	protected void btnGoBackListe_Click(object sender, EventArgs e)
-	{
-		int idLog = iZyInt.ConvertStringToInt(MySession.GetParam("idLog"));
-		Log myLog = LogManager.Load(idLog);
-		if (myLog != null)
-		{
-			DisplayListeFichiers(myLog.cheminFichier);
-			ScriptManager.RegisterStartupScript(this, this.GetType(), "HideFileDelay", "HideFileDelay();", true);
-			upGeneral.Update();
-		}
-	}
+    protected void DisplayListeFichiers(string cheminRepertoire)
+    {
+        if (Directory.Exists(cheminRepertoire))
+        {
+            DirectoryInfo dir = new DirectoryInfo(cheminRepertoire);
+            FileInfo[] arrayFichiers = dir.GetFiles();
+            
+
+
+            List<Fichier> listeFichiers = arrayFichiers.Select(f => new Fichier() { chemin = f.FullName, nom = f.Name, taille = ConvertOctets(f.Length) }).ToList();
+            rptListeFichiers.DataSource = listeFichiers.OrderBy(f => f.nom);
+            rptListeFichiers.DataBind();
+
+
+            DirectoryInfo[] listeFolder = dir.GetDirectories();
+            List<Repertoire> listeRepertoire = listeFolder.Select(d => new Repertoire() { chemin = d.FullName, nom = d.Name }).ToList();
+            rptListeFolder.DataSource = listeRepertoire.OrderBy(f => f.nom);
+            rptListeFolder.DataBind();
+        }
+    }
+
+    protected void btnSeeFichier_Click(object sender, EventArgs e)
+    {
+        LinkButton btn = (LinkButton)sender;
+        if (btn != null && !string.IsNullOrEmpty(btn.CommandArgument))
+        {
+            DisplayFichierContenu(btn.CommandArgument);
+
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "ShowFileDelay", "ShowFileDelay();", true);
+            upFile.Update();
+        }
+    }
+    protected void btnGoFolder_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnGoBackListe_Click(object sender, EventArgs e)
+    {
+        int idLog = iZyInt.ConvertStringToInt(MySession.GetParam("idLog"));
+        Log myLog = LogManager.Load(idLog);
+        if (myLog != null)
+        {
+            DisplayListeFichiers(myLog.cheminFichier);
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "HideFileDelay", "HideFileDelay();", true);
+            upGeneral.Update();
+        }
+    }
+
+
+    #region TOOLS
+    protected string ConvertOctets(long octets)
+    {
+        string res = string.Empty;
+        long L_Size = octets;
+
+        if (L_Size / 1024 > 1)
+        {
+            L_Size = L_Size / 1024;
+            if (L_Size / 1024 > 1)
+            {
+                L_Size = L_Size / 1024;
+                if (L_Size / 1024 > 1)
+                {
+                    L_Size = L_Size / 1024;
+                    if (L_Size / 1024 > 1)
+                    {
+                        L_Size = L_Size / 1024;
+                    }
+                    else
+                    {
+                        res = L_Size + " Go";
+                    }
+                }
+                else
+                {
+                    res = L_Size + " Mo";
+                }
+
+            }
+            else
+            {
+                res = L_Size + " Ko";
+            }
+        }
+        else
+        {
+            res = L_Size + " Oct";
+        }
+        return res;
+    }
+
+    #endregion
 }
